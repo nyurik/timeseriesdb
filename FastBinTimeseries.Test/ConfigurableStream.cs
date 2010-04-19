081b2bdbf992174df024c199339763eb2e6cc66d
yuriastrakhan <yuriastrakhan@537f4964-0365-11de-bcc6-d9108be23e13>
2010-04-19 20:22:50 +0000

    Missed in the last checkin - fake stream and streaming tests
    git-svn-id: https://timeseriesdb.googlecode.com/svn/trunk@30 537f4964-0365-11de-bcc6-d9108be23e13

---
+++ FastBinTimeseries.Test/ConfigurableStream.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+
+namespace NYurik.FastBinTimeseries.Test
+{
+    public class ConfigurableStream : Stream
+    {
+        private readonly Stream _stream;
+
+        public ConfigurableStream(Stream stream)
+        {
+            _stream = stream;
+            AllowSeek = true;
+            AllowRead = true;
+            AllowWrite = true;
+        }
+
+        public bool AllowSeek { get; set; }
+        public bool AllowRead { get; set; }
+        public bool AllowWrite { get; set; }
+
+
+        public override bool CanRead
+        {
+            get { return AllowRead && _stream.CanRead; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return AllowSeek && _stream.CanSeek; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return AllowWrite && _stream.CanWrite; }
+        }
+
+        public override long Length
+        {
+            get
+            {
+                RequestSeek();
+                return _stream.Length;
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                RequestSeek();
+                return _stream.Position;
+            }
+            set
+            {
+                RequestSeek();
+                _stream.Position = value;
+            }
+        }
+
+        private void RequestSeek()
+        {
+            if (!AllowSeek) throw new NotSupportedException();
+        }
+
+        private void RequestRead()
+        {
+            if (!AllowRead) throw new NotSupportedException();
+        }
+
+        private void RequestWrite()
+        {
+            if (!AllowWrite) throw new NotSupportedException();
+        }
+
+        public override void Flush()
+        {
+            _stream.Flush();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            RequestSeek();
+            return _stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            RequestSeek();
+            _stream.SetLength(value);
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            RequestRead();
+            return _stream.Read(buffer, offset, count);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            RequestWrite();
+            _stream.Write(buffer, offset, count);
+        }
+    }
+}
---
+++ FastBinTimeseries.Test/StreamingTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace NYurik.FastBinTimeseries.Test
+{
+    [TestFixture]
+    public class StreamingTests : TestsBase
+    {
+        [Test]
+        public void StreamingTest()
+        {
+            string fileName = GetBinFileName();
+
+            byte[] data = TestUtils.GenerateData<byte>(NewByte, 10000, 0);
+            if (AllowCreate)
+            {
+                using (var b = new BinIndexedFile<byte>(fileName))
+                {
+                    b.InitializeNewFile();
+                    b.WriteData(0, new ArraySegment<byte>(data));
+                }
+            }
+
+            byte[] bytes = File.ReadAllBytes(fileName);
+
+            using (var b = (BinIndexedFile<byte>) BinaryFile.Open(fileName, false))
+            {
+                var ms = new MemoryStream(bytes);
+                var cs = new ConfigurableStream(ms);
+                var data2 = new byte[data.Length/2];
+
+                cs.AllowSeek = cs.AllowWrite = false;
+                var b2 = (BinIndexedFile<byte>) BinaryFile.Open(cs, null);
+                Assert.IsTrue(b2.IsOpen);
+                Assert.AreEqual(b.ItemSize, b2.ItemSize);
+
+                b2.ReadData(0, new ArraySegment<byte>(data2));
+                CollectionAssert.AreEqual(TestUtils.GenerateData<byte>(NewByte, data.Length/2, 0), data2);
+
+                b2.ReadData(0, new ArraySegment<byte>(data2));
+                CollectionAssert.AreEqual(TestUtils.GenerateData<byte>(NewByte, data.Length/2, data.Length/2), data2);
+            }
+        }
+
+        private static byte NewByte(long i)
+        {
+            return (byte) (i%byte.MaxValue);
+        }
+    }
+}
