6665e9cfa9b0665ae8aa83f4ac8ac2fda8591f0d
yuriastrakhan@gmail.com <yuriastrakhan@gmail.com@537f4964-0365-11de-bcc6-d9108be23e13>
2011-11-23 17:46:09 +0000

    planned out dynamic de-serialization
    git-svn-id: https://timeseriesdb.googlecode.com/svn/trunk@46 537f4964-0365-11de-bcc6-d9108be23e13

--- FastBinTimeseries/Serializers/BlockSerializer/DynamicSerializer.cs
+++ FastBinTimeseries/Serializers/BlockSerializer/DynamicSerializer.cs
@@ -9,9 +9,15 @@ namespace NYurik.FastBinTimeseries.Serializers.BlockSerializer
     internal static class DynamicSerializer<T>
     {
         ///
-        /// Expected code:
+        /// Generated code:
         /// 
-        /// bool Func(StreamCodec codec, IEnumerator&ltT> enumerator)
+        /// 
+        /// * codec - writes output to this codec using Write*() methods
+        /// * enumerator to go through the input T values.
+        ///     MoveNext() had to be called on it before passing it in.
+        /// * returns false if no more items, or true if there are more items but the codec buffer is full
+        /// 
+        /// bool Serialize(StreamCodec codec, IEnumerator&ltT> enumerator)
         /// {
         ///     bool moveNext;
         ///     T current = enumerator.Current;
@@ -58,7 +64,7 @@ namespace NYurik.FastBinTimeseries.Serializers.BlockSerializer
             BinaryExpression setCurrentValue = Expression.Assign(
                 currentExp, Expression.PropertyOrField(enumeratorExp, "Current"));
 
-            LabelTarget breakOuter = Expression.Label();
+            LabelTarget breakLabel = Expression.Label();
 
             var stateVariables = new List<ParameterExpression> {currentExp, moveNextExp};
             var initBlock = new List<Expression>();
@@ -105,7 +111,7 @@ namespace NYurik.FastBinTimeseries.Serializers.BlockSerializer
                                     // (!moveNext)
                                     Expression.IsFalse(moveNextExp),
                                     // break;
-                                    Expression.Break(breakOuter)),
+                                    Expression.Break(breakLabel)),
                                 // current = enumerator.Current;
                                 setCurrentValue,
                                 Expression.Block(
@@ -120,11 +126,11 @@ namespace NYurik.FastBinTimeseries.Serializers.BlockSerializer
                                         // ReSharper restore AssignNullToNotNullAttribute
                                         Expression.Block(
                                             Expression.Assign(codecBufferPos, codecPosExp),
-                                            Expression.Break(breakOuter)
+                                            Expression.Break(breakLabel)
                                             )
                                         ))
                             }),
-                    breakOuter));
+                    breakLabel));
 
             // return moveNext;
             methodBody.Add(moveNextExp);
@@ -140,9 +146,80 @@ namespace NYurik.FastBinTimeseries.Serializers.BlockSerializer
             return serializeExp.Compile();
         }
 
-        public static Func<StreamCodec, IEnumerator<T>, bool> DeSerialize(IEnumerable<TypeSerializer> serializers)
+        ///
+        /// Generated code:
+        /// 
+        /// * codec to read the values from
+        /// * resultBuffer will get all the generated values
+        /// * maxItemCount - maximum number of items to be deserialized
+        /// 
+        /// void DeSerialize(StreamCodec codec, Buff&lt;T> resultBuffer, int maxItemCount)
+        /// {
+        ///     int itemCount = codec.ItemCount;
+        ///     if (itemCount > maxItemCount)
+        ///         itemCount = maxItemCount;
+        ///     
+        ///     var state1, state2, ...;
+        /// 
+        ///     codec.Read(state1);
+        ///     codec.Read(state2);
+        ///     ...
+        /// 
+        ///     while (true) {
+        /// 
+        ///         itemCount--;
+        ///         if (itemCount == 0)
+        ///             break;
+        /// 
+        ///         @if T is a struct
+        ///             T newValue = new T();
+        ///         @else
+        ///             T newValue = FormatterServices.GetUninitializedObject(typeof(T)); 
+        /// 
+        ///         newValue.Field1 = ReadField1(codec, ref state1);
+        ///         newValue.Field2 = ReadField2(codec, ref state2);
+        ///         ...
+        /// 
+        ///         resultBuffer.Add(newValue);
+        ///     }
+        /// }
+        /// 
+        public static Action<StreamCodec, Buff<T>, int> DeSerialize(IEnumerable<TypeSerializer> serializers)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    internal class Buff<T>
+    {
+        private int _count;
+        private T[] _buffer;
+
+        public Buff()
+        {
+            _buffer = new T[4];
+        }
+
+        public void Add(T value)
+        {
+            if (_count == _buffer.Length)
+            {
+                var tmp = new T[_buffer.Length*2];
+                Array.Copy(_buffer, tmp, _buffer.Length);
+                _buffer = tmp;
+            }
+            _buffer[_count++] = value;
+        }
+
+        public ArraySegment<T> Buffer
+        {
+            get { return new ArraySegment<T>(_buffer, 0, _count); }
+        }
+
+        public void Reset()
+        {
+            Array.Clear(_buffer, 0, _count);
+            _count = 0;
         }
     }
 }
