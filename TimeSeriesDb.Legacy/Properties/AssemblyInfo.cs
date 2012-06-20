c9ef95262da64622dcf1575648800e9411f267cc
Yuri Astrakhan <yuriastrakhan@gmail.com>
2012-06-20 17:20:53 -0400

    Added nuget package info

--- TimeSeriesDb.Legacy/Properties/AssemblyInfo.cs
+++ TimeSeriesDb.Legacy/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 using NYurik.TimeSeriesDb.Properties;
 
 [assembly: AssemblyTitle("TimeSeriesDb.Legacy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This package contains the features of NYurik.TimeSeriesDb library that were obsoleted and removed from the main package.")]
 [assembly: AssemblyProduct("TimeSeriesDb.Legacy")]
 [assembly: ComVisible(false)]
 [assembly: Guid("58958894-a74d-4d50-bb9e-535f5a22527c")]
